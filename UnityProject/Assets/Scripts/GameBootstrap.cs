1f6ddae00671c98d7b9e41fd39db1546be8592a2
powerplayexe <nivedsreejaharidasan@gmail.com>
2026-09-06 16:40:10 +0530

    Add Unity 6.4 game bootstrap and procedural city

---
+++ UnityProject/Assets/Scripts/GameBootstrap.cs
@@ -0,0 +1,111 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class GameBootstrap : MonoBehaviour
+{
+    public Material roadMaterial, buildingMaterial, glassMaterial, grassMaterial, carMaterial, skinMaterial;
+    public int TrafficCount = 24;
+    public int PedestrianCount = 20;
+    public bool GenerateOnStart = true;
+
+    Transform world;
+    readonly List<GameObject> buildings = new();
+
+    void Start()
+    {
+        if (GenerateOnStart) BuildGame();
+    }
+
+    public void BuildGame()
+    {
+        world = new GameObject("LosSantosWorld").transform;
+        CreateMaterials();
+        CreateLighting();
+        CreateGroundAndRoads();
+        CreateBuildings();
+        CreatePlayer();
+        CreateTraffic();
+        CreatePedestrians();
+        CreateUI();
+        new GameObject("MissionManager").AddComponent<MissionManager>();
+    }
+
+    void CreateMaterials()
+    {
+        roadMaterial ??= MakeMat(new Color(.08f,.09f,.11f));
+        buildingMaterial ??= MakeMat(new Color(.42f,.40f,.38f));
+        glassMaterial ??= MakeMat(new Color(.08f,.28f,.34f), .25f, .2f);
+        grassMaterial ??= MakeMat(new Color(.18f,.28f,.17f));
+        carMaterial ??= MakeMat(new Color(.35f,.12f,.08f), .55f, .2f);
+        skinMaterial ??= MakeMat(new Color(.64f,.42f,.30f));
+    }
+
+    Material MakeMat(Color c, float smooth = .75f, float metal = 0)
+    {
+        var m = new Material(Shader.Find("Universal Render Pipeline/Lit"));
+        m.color = c; m.SetFloat("_Smoothness", smooth); m.SetFloat("_Metallic", metal); return m;
+    }
+
+    void CreateLighting()
+    {
+        var sun = new GameObject("Sun").AddComponent<Light>();
+        sun.type = LightType.Directional; sun.intensity = 1.1f; sun.transform.rotation = Quaternion.Euler(45,-35,0);
+        var sky = new GameObject("Ambient").AddComponent<Light>();
+        sky.type = LightType.Point; sky.intensity = .35f; sky.range = 100f; sky.transform.position = Vector3.up * 20f;
+    }
+
+    void CreateGroundAndRoads()
+    {
+        var ground = GameObject.CreatePrimitive(PrimitiveType.Cube); ground.name="Ground"; ground.transform.SetParent(world); ground.transform.localScale=new Vector3(240,.2f,240); ground.transform.position=new Vector3(0,-.15f,30); ground.GetComponent<Renderer>().material=grassMaterial;
+        Road("Road_EastWest_0", new Vector3(0,0,0), new Vector3(240,.08f,24));
+        Road("Road_EastWest_1", new Vector3(0,0,55), new Vector3(240,.08f,24));
+        Road("Road_NorthSouth_0", new Vector3(0,0,27), new Vector3(24,.08f,240));
+        Road("Road_NorthSouth_1", new Vector3(55,0,27), new Vector3(24,.08f,240));
+    }
+
+    void Road(string n, Vector3 p, Vector3 s){var r=GameObject.CreatePrimitive(PrimitiveType.Cube);r.name=n;r.transform.SetParent(world);r.transform.position=p;r.transform.localScale=s;r.GetComponent<Renderer>().material=roadMaterial;}
+
+    void CreateBuildings()
+    {
+        var data=new[]{
+            new Vector3(-45,0,-42),new Vector3(28,0,-42),new Vector3(87,0,-42),
+            new Vector3(-45,0,28),new Vector3(28,0,28),new Vector3(87,0,28),
+            new Vector3(-87,0,90),new Vector3(45,0,95)
+        };
+        for(int i=0;i<data.Length;i++) CreateBuilding($"Building_{i+1:00}",data[i],new Vector3(20,10+i%3*5,20));
+    }
+
+    void CreateBuilding(string n, Vector3 p, Vector3 size)
+    {
+        var b=GameObject.CreatePrimitive(PrimitiveType.Cube);b.name=n;b.transform.SetParent(world);b.transform.position=p+Vector3.up*size.y*.5f;b.transform.localScale=size;b.GetComponent<Renderer>().material=buildingMaterial;
+        buildings.Add(b);
+        var tag=b.AddComponent<BuildingInterior>(); tag.BuildingName=n.ToUpper();
+        var door=GameObject.CreatePrimitive(PrimitiveType.Cube);door.name="Entrance";door.transform.SetParent(b.transform);door.transform.localPosition=new Vector3(0,-size.y*.5f+.9f,size.z*.5f+.08f);door.transform.localScale=new Vector3(2.2f,2.8f,.2f);door.GetComponent<Renderer>().material=glassMaterial;
+    }
+
+    void CreatePlayer()
+    {
+        var p=GameObject.CreatePrimitive(PrimitiveType.Capsule);p.name="Player";p.transform.SetParent(world);p.transform.position=new Vector3(0,1.1f,10);p.layer=0;var cc=p.AddComponent<CharacterController>();cc.height=2.2f;cc.radius=.42f;Object.Destroy(p.GetComponent<CapsuleCollider>());p.AddComponent<PlayerController>();
+        var cam=new GameObject("PlayerCamera");var c=cam.AddComponent<Camera>();c.tag="MainCamera";cam.AddComponent<AudioListener>();cam.transform.SetParent(p.transform);cam.transform.localPosition=new Vector3(0,2.0f,-6.5f);cam.transform.localEulerAngles=new Vector3(12,0,0);var follow=cam.AddComponent<ThirdPersonCamera>();follow.Target=p.transform;
+        p.AddComponent<WeaponSystem>();
+    }
+
+    void CreateTraffic(){for(int i=0;i<TrafficCount;i++){bool h=i%2==0;float t=-100+Random.value*200;Vector3 pos=h?new Vector3(t,.5f,(i%4<2)?-5.5f:6f):new Vector3((i%4<2)?-5.5f:6f,.5f,t);float yaw=h?(i%4<2?90:-90):(i%4<2?0:180);CreateCar($"TrafficCar_{i:00}",pos,yaw,true);}}
+
+    void CreateCar(string n,Vector3 p,float yaw,bool ai)
+    {
+        var car=new GameObject(n);car.transform.SetParent(world);car.transform.position=p;car.transform.rotation=Quaternion.Euler(0,yaw,0);var rb=car.AddComponent<Rigidbody>();rb.mass=1500;rb.drag=.25f;rb.angularDrag=3f;rb.interpolation=RigidbodyInterpolation.Interpolate;var body=car.AddComponent<BoxCollider>();body.size=new Vector3(2,1,4);body.center=new Vector3(0,.2f,0);
+        var bodyMesh=GameObject.CreatePrimitive(PrimitiveType.Cube);bodyMesh.name="Body";bodyMesh.transform.SetParent(car.transform);bodyMesh.transform.localPosition=new Vector3(0,.55f,0);bodyMesh.transform.localScale=new Vector3(2,1,4);bodyMesh.GetComponent<Renderer>().material=carMaterial;Object.Destroy(bodyMesh.GetComponent<BoxCollider>());
+        var vc=car.AddComponent<VehicleController>();vc.PlayerVehicle=!ai;vc.SetupWheels(carMaterial);
+        if(ai)car.AddComponent<TrafficAI>();
+    }
+
+    void CreatePedestrians(){for(int i=0;i<PedestrianCount;i++){var npc=GameObject.CreatePrimitive(PrimitiveType.Capsule);npc.name=$"Pedestrian_{i:00}";npc.transform.SetParent(world);npc.transform.position=new Vector3(Random.Range(-105,105),1,Random.Range(-100,130));Object.Destroy(npc.GetComponent<CapsuleCollider>());npc.AddComponent<CharacterAI>();}}
+
+    void CreateUI()
+    {
+        var canvas=new GameObject("HUD").AddComponent<Canvas>();canvas.renderMode=RenderMode.ScreenSpaceOverlay;canvas.gameObject.AddComponent<CanvasScaler>();canvas.gameObject.AddComponent<GraphicRaycaster>();
+        var txt=new GameObject("Hint").AddComponent<Text>();txt.transform.SetParent(canvas.transform);txt.font=Resources.GetBuiltinResource<Font>("LegacyRuntime.ttf");txt.fontSize=18;txt.color=Color.white;txt.alignment=TextAnchor.LowerLeft;txt.text="WASD Move  |  Mouse Camera  |  LMB Shoot  |  E Enter  |  F Exit  |  1/2/3 Weapons  |  Space Jump  |  J Mission";var rt=txt.rectTransform;rt.anchorMin=new Vector2(0,0);rt.anchorMax=new Vector2(1,0.1f);rt.offsetMin=new Vector2(20,10);rt.offsetMax=new Vector2(-20,0);
+    }
+}
