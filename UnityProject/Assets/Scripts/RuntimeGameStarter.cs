aa9bb7fa563c3781c6939c720af52747e528ad80
powerplayexe <nivedsreejaharidasan@gmail.com>
2026-09-06 17:13:14 +0530

    Make Unity runtime bootstrap explicit and idempotent

--- UnityProject/Assets/Scripts/RuntimeGameStarter.cs
+++ UnityProject/Assets/Scripts/RuntimeGameStarter.cs
@@ -5,14 +5,20 @@ public static class RuntimeGameStarter
     [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]
     static void StartGame()
     {
-        if (Object.FindFirstObjectByType<GameBootstrap>() != null)
+        var bootstrap = Object.FindFirstObjectByType<GameBootstrap>();
+        if (bootstrap != null)
+        {
+            bootstrap.GenerateOnStart = false;
+            bootstrap.BuildGame();
             return;
+        }
 
         var root = new GameObject("LOS SANTOS GAME");
         Object.DontDestroyOnLoad(root);
-        var bootstrap = root.AddComponent<GameBootstrap>();
+        bootstrap = root.AddComponent<GameBootstrap>();
         bootstrap.TrafficCount = 24;
         bootstrap.PedestrianCount = 20;
-        bootstrap.GenerateOnStart = true;
+        bootstrap.GenerateOnStart = false;
+        bootstrap.BuildGame();
     }
 }
