97c824a3ea27cdbbe44cf1a9a779e45998afb943
powerplayexe <nivedsreejaharidasan@gmail.com>
2026-09-06 16:41:04 +0530

    Use Unity Input System and building interaction

--- UnityProject/Assets/Scripts/PlayerController.cs
+++ UnityProject/Assets/Scripts/PlayerController.cs
@@ -1,33 +1,10 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(CharacterController))]
-public class PlayerController : MonoBehaviour
-{
-    public float WalkSpeed = 5f;
-    public float SprintSpeed = 8.5f;
-    public float Gravity = -22f;
-    public float JumpHeight = 1.5f;
-    public Transform CameraRoot;
-    CharacterController controller;
-    Camera cam;
-    float verticalVelocity;
-
-    void Awake(){controller=GetComponent<CharacterController>();cam=Camera.main;if(!CameraRoot&&cam)CameraRoot=cam.transform;}
-
-    void Update()
-    {
-        if(GetComponent<VehicleController>()?.IsDriven==true) return;
-        Vector3 f=CameraRoot?CameraRoot.forward:transform.forward; f.y=0; f.Normalize();
-        Vector3 r=CameraRoot?CameraRoot.right:transform.right; r.y=0; r.Normalize();
-        Vector3 input=new Vector3(Input.GetAxisRaw("Horizontal"),0,Input.GetAxisRaw("Vertical"));
-        input=Vector3.ClampMagnitude(input,1);
-        Vector3 move=(f*input.z+r*input.x);
-        float speed=Input.GetKey(KeyCode.LeftShift)?SprintSpeed:WalkSpeed;
-        controller.Move(move*speed*Time.deltaTime);
-        if(move.sqrMagnitude>.01f) transform.rotation=Quaternion.Slerp(transform.rotation,Quaternion.LookRotation(move),12f*Time.deltaTime);
-        if(controller.isGrounded&&verticalVelocity<0) verticalVelocity=-2f;
-        if(Input.GetKeyDown(KeyCode.Space)&&controller.isGrounded) verticalVelocity=Mathf.Sqrt(JumpHeight*-2f*Gravity);
-        verticalVelocity+=Gravity*Time.deltaTime;
-        controller.Move(Vector3.up*verticalVelocity*Time.deltaTime);
-    }
+public class PlayerController:MonoBehaviour{
+ public float WalkSpeed=5, SprintSpeed=8.5f, Gravity=-22, JumpHeight=1.5f; CharacterController cc; Transform cam; float vy;
+ void Awake(){cc=GetComponent<CharacterController>();cam=Camera.main?Camera.main.transform:null;}
+ void Update(){Keyboard k=Keyboard.current;if(k==null||!gameObject.activeSelf)return;Vector2 i=new Vector2((k.dKey.isPressed?1:0)-(k.aKey.isPressed?1:0),(k.wKey.isPressed?1:0)-(k.sKey.isPressed?1:0));i=Vector2.ClampMagnitude(i,1);Vector3 f=cam?cam.forward:transform.forward,r=cam?cam.right:transform.right;f.y=0;r.y=0;f.Normalize();r.Normalize();Vector3 move=f*i.y+r*i.x;float sp=k.leftShiftKey.isPressed?SprintSpeed:WalkSpeed;cc.Move(move*sp*Time.deltaTime);if(move.sqrMagnitude>.01f)transform.rotation=Quaternion.Slerp(transform.rotation,Quaternion.LookRotation(move),12*Time.deltaTime);if(cc.isGrounded&&vy<0)vy=-2;if(k.spaceKey.wasPressedThisFrame&&cc.isGrounded)vy=Mathf.Sqrt(JumpHeight*-2*Gravity);vy+=Gravity*Time.deltaTime;cc.Move(Vector3.up*vy*Time.deltaTime);if(k.eKey.wasPressedThisFrame)TryBuilding();}
+ void TryBuilding(){var bs=FindObjectsByType<BuildingInterior>(FindObjectsSortMode.None);BuildingInterior best=null;float d=4;foreach(var b in bs){float q=Vector3.Distance(transform.position,b.transform.position);if(q<d){d=q;best=b;}}if(best)best.Toggle(this);}
 }
